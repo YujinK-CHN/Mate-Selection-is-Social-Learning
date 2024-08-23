4e8aabd2e40a1146143a7c5637267f74673fa8c1
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-08-23 13:30:44 -0400

    1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pettingzoo.sisl import waterworld_v4, multiwalker_v9
 from pettingzoo.mpe import simple_spread_v3
 import gymnasium as gym
 
@@ -52,7 +51,7 @@ if __name__ == "__main__":
     """ALGO PARAMS"""
     config = {
         'device': torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-        'env_name': "Boxing",
+        'env_name': "HalfCheetah",
         'obs_shape': None,
         'num_actions': None,
         'continuous': None,
