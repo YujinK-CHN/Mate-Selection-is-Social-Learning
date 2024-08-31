5c9e56cc51b7cfc4d123c59162518ad1b4ccaf40
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-08-31 16:50:47 -0400

    1

--- pipline/train.py
+++ pipline/train.py
@@ -2,6 +2,6 @@ def training(config, algo):
     x, y = algo.train()
 
     if algo.name == 'mtppo':
-        algo.save(f"./models/{config['hidden_size']}_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}.pt")
+        algo.save(f"./models/{config['hidden_size']}_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}_seed{algo.seed}.pt")
 
     return x, y
