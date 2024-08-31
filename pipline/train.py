e425b39d43cf4a08b9b84b7e5b03c71913274482
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-08-31 14:03:41 -0400

    1

--- pipline/train.py
+++ pipline/train.py
@@ -1,10 +1,6 @@
 def training(config, algo_list):
     for algo in algo_list:
         algo.train()
-        
-        if algo.name == 'ippo' or algo.name == 'mappo':
-            algo.save(f"./models/{config['env_name']}_{algo.name}_{config['pop_size']}_{config['max_cycles']}_{config['total_episodes']}.pt")
-        if algo.name == 'ppo':
-            algo.save(f"./models/{algo.name}_{config['pop_size']}_{config['max_cycles']}_{config['total_episodes']}.pt")
+
         if algo.name == 'mtppo':
-            algo.save(f"./models/{config['pop_size']}_{config['max_cycles']}_{config['total_episodes']}.pt")
+            algo.save(f"./models/{config['hidden_size']}_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}.pt")
