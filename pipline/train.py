c9a81f93af77e22375bd65ddb593c649b15d7357
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-09-09 14:41:46 -0400

    1

--- pipline/train.py
+++ pipline/train.py
@@ -7,7 +7,7 @@ def training(config, algo):
         formatted_date = date.today().strftime("%d-%m-%Y")
         algo.save(f"./models/{config['hidden_size']}_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}_seed{algo.seed}.pt")
         np.save(f"./models/train_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}_seed{algo.seed}_{formatted_date}.npy", np.array(y))
-        np.save(f"./models/eval_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}_seed{algo.seed}_{formatted_date}.npy", np.array(y_eval))
+        np.save(f"./logs/eval_{config['batch_size']}_{config['epoch_opt']}_{config['total_episodes']}_seed{algo.seed}_{formatted_date}.npy", np.array(y_eval))
         return x, y, x_eval, y_eval
 
     if algo.name == 'sle-mtppo':
