fd2c2233c82c41566219c9adbd03273d1aa358f3
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-09-26 23:25:28 -0400

    1

--- show.py
+++ show.py
@@ -76,21 +76,32 @@ config = {
 
 
 x = [i for i in range(200)]
+MT = 3
+POP = 3
 seed = 861
-eval_returns = np.load(f"logs/sle-mtppo_3tasks_3agents_30000_100_2024-09-26/{seed}/eval_returns.npy")
-eval_sr = np.load(f"logs/sle-mtppo_3tasks_3agents_30000_100_2024-09-26/{seed}/eval_sr.npy")
-eval_tasks_sr = np.load(f"logs/sle-mtppo_3tasks_3agents_30000_100_2024-09-26/{seed}/eval_tasks_sr.npy")
+date = '2024-09-26'
+eval_returns = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/eval_returns.npy")
+eval_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/eval_sr.npy")
+eval_tasks_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/eval_tasks_sr.npy")
+training_returns = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/training_returns.npy")
+training_tasks_return = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/training_tasks_return.npy")
+training_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/training_sr.npy")
+training_tasks_sr = np.load(f"logs/sle-mtppo_{MT}tasks_3agents_30000_100_{date}/{seed}/training_tasks_sr.npy")
 
 print(eval_returns)
 print(eval_sr)
 print(eval_tasks_sr)
-
+print(training_returns)
+print(training_tasks_return)
+print(training_sr)
+print(training_tasks_sr)
+'''
 plt.plot(x, eval_sr)
 plt.title(f"Episode returns (train and eval) for seed {seed}")
 plt.xlabel("Episodes")
 plt.ylabel("Success Rate")
 plt.show()
 
-
+'''
 
 
