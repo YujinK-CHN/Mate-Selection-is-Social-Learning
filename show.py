9221e7d87d95f4b3279d59412a45415662dc04e8
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-10-04 12:47:01 -0400

    1

--- show.py
+++ show.py
@@ -14,15 +14,15 @@ import numpy as np
 x = [i for i in range(100)]
 MT = 3
 POP = 3
-seed = 788
-date = '2024-10-02'
-eval_returns = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/eval_returns.npy")
-eval_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/eval_sr.npy")
-eval_tasks_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/eval_tasks_sr.npy")
-training_returns = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/training_returns.npy")
-training_tasks_return = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/training_tasks_return.npy")
-training_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}/training_sr.npy")
-training_tasks_sr = np.load(f"logs/sle-mtppo_{MT}tasks_3agents_30000_100_{date}/{seed}/training_tasks_sr.npy")
+seed = 82
+date = '2024-10-04'
+eval_returns = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}(done)/eval_returns.npy")
+eval_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}(done)/eval_sr.npy")
+eval_tasks_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}(done)/eval_tasks_sr.npy")
+training_returns = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}(done)/training_returns.npy")
+training_tasks_return = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}(done)/training_tasks_return.npy")
+training_sr = np.load(f"logs/sle-mtppo_{MT}tasks_{POP}agents_30000_100_{date}/{seed}(done)/training_sr.npy")
+training_tasks_sr = np.load(f"logs/sle-mtppo_{MT}tasks_3agents_30000_100_{date}/{seed}(done)/training_tasks_sr.npy")
 
 print(eval_returns.shape)
 print(eval_sr.shape)
@@ -32,7 +32,7 @@ print(training_tasks_return.shape)
 print(training_sr.shape)
 print(training_tasks_sr.shape)
 
-plt.plot(x, np.max(eval_sr, axis=-1))
+plt.plot(x, np.max(training_sr, axis=-1))
 plt.title(f"Episode returns (train and eval) for seed {seed}")
 plt.xlabel("Episodes")
 plt.ylabel("Success Rate")
