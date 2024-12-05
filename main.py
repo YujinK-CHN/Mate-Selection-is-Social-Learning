b4e4db9e5135422113efd260124603b38dee3a12
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2024-12-05 16:46:36 -0500

    1

--- main.py
+++ main.py
@@ -208,12 +208,12 @@ if __name__ == "__main__":
     """ ENV SETUP """
     # Random Seed(0): [0] 788x [1] 861 [2] 82 [3] 530 [4] 995 [5] 829
     seeds = random_seeds()
-    #seeds_ppo = seeding('mtppo', seeds, config_mtppo)
+    seeds_ppo = seeding('mtppo', seeds, config_mtppo)
     #run_seeds(seeds_ppo)
     
     total_start_time = time.time()
-    #training(config_mtppo, seeds_ppo[1])
-    training(config, SLE_MTPPO(MultiTaskEnv(seeds[2]), config))
+    training(config_mtppo, seeds_ppo[2])
+    #training(config, SLE_MTPPO(MultiTaskEnv(seeds[2]), config))
     total_end_time = time.time()
     total_duration = total_end_time - total_start_time
     print(f"Total training runtime: {total_duration:.2f} seconds")
